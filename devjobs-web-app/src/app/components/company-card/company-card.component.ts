97dd5d2968cc208d0158d6a16001b260e6fc6e18
lucasalmeida <lac.lucas96@gmail.com>
2023-12-15 19:04:32 -0300

    Styles job page content

--- devjobs-web-app/src/app/components/company-card/company-card.component.ts
+++ devjobs-web-app/src/app/components/company-card/company-card.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Company } from '../../@types/Company';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-company-card',
@@ -10,4 +13,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './company-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CompanyCardComponent { }
+export class CompanyCardComponent {
+  @Input({ required: true }) company!: Company;
+
+  @ViewChild('logo') logo!: ElementRef<HTMLDivElement>;
+
+  API_URL = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.getSvgLogo();
+  }
+
+  getSvgLogo() {
+    const headers = new HttpHeaders();
+    headers.set('Accept', 'image/svg+xml');
+    const svgString = this.httpClient.get(`${this.API_URL}/logos/${this.company.name.toLowerCase()}.svg`, { headers, responseType: 'text' }).subscribe({
+      next: (v) => {
+        const parser = new DOMParser();
+        const { documentElement } = parser.parseFromString(v, "image/svg+xml");
+        this.logo.nativeElement.appendChild(documentElement);
+      }
+    });
+
+    return svgString;
+  }
+}
